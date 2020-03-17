10719ce8cc05cea1cf44eae5da3d10d13b61ca0e
__ALUCARD__ <53409082+xanthe1337@users.noreply.github.com>
2020-03-17 14:44:38 +0100

    github gay about indenting but kek

--- Slotbot/Slotbot.py
+++ Slotbot/Slotbot.py
@@ -6,7 +6,7 @@ init(convert=True)
 
 client = discord.Client()
 
-with open('config.json) as f:
+with open('config.json') as f:
   config = json.load(f)
 
 token = config.get('token')
@@ -14,8 +14,8 @@ token = config.get('token')
 @client.event
 async def on_message(message):
     if 'Someone just dropped' in message.content:
-       if message.author.id == 346353957029019648:
-          await message.channel.send('~grab')
-       print(f"Stole some slotbot currency in: {Fore.GREEN}{message.channel}{Fore.RESET} | {Fore.MAGENTA}{message.guild}{Fore.RESET}")
+        if message.author.id == 346353957029019648:
+            await message.channel.send('~grab')
+        print(f"Stole some slotbot currency in: {Fore.GREEN}{message.channel}{Fore.RESET} | {Fore.MAGENTA}{message.guild}{Fore.RESET}")
        
-client.run(token, bot=False)       
+client.run(token, bot=False)
