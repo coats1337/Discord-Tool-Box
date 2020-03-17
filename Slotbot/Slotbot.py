16420a22562b9b5a01d28eaf17071687529c8397
__ALUCARD__ <53409082+xanthe1337@users.noreply.github.com>
2020-03-17 14:42:27 +0100

    Update Slotbot.py

--- Slotbot/Slotbot.py
+++ Slotbot/Slotbot.py
@@ -16,6 +16,6 @@ async def on_message(message):
     if 'Someone just dropped' in message.content:
        if message.author.id == 346353957029019648:
           await message.channel.send('~grab')
-       print(f"Stole some slotbot currency in: {Fore.GREEN}{message.channel}{Fore.RESET}  | {Fore.MAGENTA}{message.guild}{Fore.RESET}")
+       print(f"Stole some slotbot currency in: {Fore.GREEN}{message.channel}{Fore.RESET} | {Fore.MAGENTA}{message.guild}{Fore.RESET}")
        
 client.run(token, bot=False)       
